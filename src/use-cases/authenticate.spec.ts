1d1ffbdded2ec72021664450f2b1e5c319f651fd
luanrobert07 <luan.robert@gec.inatel.br>
2024-02-14 22:52:13 -0300

    teste unitário authenticate funcional

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -1,18 +1,21 @@
+import { InMemoryUsersRepository } from '@/repository/in-memory/in-memory-users-repository'
+import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { hash } from 'bcryptjs'
 import { expect, describe, it, beforeEach } from 'vitest'
-import { AuthenticateUseCase } from './authenticate'
-import { InMemoryUsersRepository } from '@/repository/in-memory/in-memory-users-repository'
 import { InvalidCredentialsError } from './errors/invalid.credentials-error'
 
 let usersRepository: InMemoryUsersRepository
 let sut: AuthenticateUseCase
 
 describe('Authenticate Use Case', () => {
+
+  beforeEach(()=>{
+    usersRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(usersRepository)
+  })
+
   it('should be able to authenticate', async () => {
-    beforeEach(() => {
-      usersRepository = new InMemoryUsersRepository()
-      sut = new AuthenticateUseCase(usersRepository)
-    })
+
 
     await usersRepository.create({
       name: 'John Doe',
@@ -29,9 +32,10 @@ describe('Authenticate Use Case', () => {
   })
 
   it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
 
-
-    await expect(() =>
+    expect(() =>
       sut.execute({
         email: 'johndoe@example.com',
         password: '123456',
@@ -39,8 +43,9 @@ describe('Authenticate Use Case', () => {
     ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 
-  it('should not be able to authenticate with wrong password', async () => {
-
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
 
     await usersRepository.create({
       name: 'John Doe',
@@ -48,7 +53,7 @@ describe('Authenticate Use Case', () => {
       password_hash: await hash('123456', 6),
     })
 
-    await expect(() =>
+    expect(() =>
       sut.execute({
         email: 'johndoe@example.com',
         password: '123123',
