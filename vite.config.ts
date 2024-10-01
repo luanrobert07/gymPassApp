c967a8964be985f35b19822bf4db5b8f00f5f8a5
luanrobert07 <luan.robert@gec.inatel.br>
2024-09-30 21:39:15 -0300

    Atualizar relatório de testes

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    reporters: ['default', 'junit'],  // Adiciona reporter JUnit
+    outputFile: 'test-results.html',   // Especifica o arquivo de saída do relatório
+    coverage: {
+      reporter: ['text', 'json', 'html'],  // Gera relatórios de cobertura em vários formatos
+    },
   },
-})
+});
